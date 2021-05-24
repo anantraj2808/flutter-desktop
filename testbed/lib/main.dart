c081082cb4b8837f4fb899e1d0aec9b9701ee2a6
Anant Raj <anantaditya6@gmail.com>
2021-05-24 12:09:00 +0530

    Initial commit

--- testbed/lib/main.dart
+++ testbed/lib/main.dart
@@ -17,8 +17,6 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:menubar/menubar.dart';
-import 'package:window_size/window_size.dart' as window_size;
 
 import 'keyboard_test_page.dart';
 
@@ -26,22 +24,22 @@ void main() {
   // Try to resize and reposition the window to be half the width and height
   // of its screen, centered horizontally and shifted up from center.
   WidgetsFlutterBinding.ensureInitialized();
-  window_size.getWindowInfo().then((window) {
-    final screen = window.screen;
-    if (screen != null) {
-      final screenFrame = screen.visibleFrame;
-      final width = math.max((screenFrame.width / 2).roundToDouble(), 800.0);
-      final height = math.max((screenFrame.height / 2).roundToDouble(), 600.0);
-      final left = ((screenFrame.width - width) / 2).roundToDouble();
-      final top = ((screenFrame.height - height) / 3).roundToDouble();
-      final frame = Rect.fromLTWH(left, top, width, height);
-      window_size.setWindowFrame(frame);
-      window_size.setWindowMinSize(Size(0.8 * width, 0.8 * height));
-      window_size.setWindowMaxSize(Size(1.5 * width, 1.5 * height));
-      window_size
-          .setWindowTitle('Flutter Testbed on ${Platform.operatingSystem}');
-    }
-  });
+//  window_size.getWindowInfo().then((window) {
+//    final screen = window.screen;
+//    if (screen != null) {
+//      final screenFrame = screen.visibleFrame;
+//      final width = math.max((screenFrame.width / 2).roundToDouble(), 800.0);
+//      final height = math.max((screenFrame.height / 2).roundToDouble(), 600.0);
+//      final left = ((screenFrame.width - width) / 2).roundToDouble();
+//      final top = ((screenFrame.height - height) / 3).roundToDouble();
+//      final frame = Rect.fromLTWH(left, top, width, height);
+//      window_size.setWindowFrame(frame);
+//      window_size.setWindowMinSize(Size(0.8 * width, 0.8 * height));
+//      window_size.setWindowMaxSize(Size(1.5 * width, 1.5 * height));
+//      window_size
+//          .setWindowTitle('Flutter Testbed on ${Platform.operatingSystem}');
+//    }
+//  });
 
   runApp(new MyApp());
 }
@@ -85,65 +83,65 @@ class _AppState extends State<MyApp> {
 
   /// Rebuilds the native menu bar based on the current state.
   void updateMenubar() {
-    setApplicationMenu([
-      Submenu(label: 'Color', children: [
-        MenuItem(
-            label: 'Reset',
-            enabled: _primaryColor != Colors.blue,
-            shortcut: LogicalKeySet(
-                LogicalKeyboardKey.meta, LogicalKeyboardKey.backspace),
-            onClicked: () {
-              setPrimaryColor(Colors.blue);
-            }),
-        MenuDivider(),
-        Submenu(label: 'Presets', children: [
-          MenuItem(
-              label: 'Red',
-              enabled: _primaryColor != Colors.red,
-              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
-                  LogicalKeyboardKey.shift, LogicalKeyboardKey.keyR),
-              onClicked: () {
-                setPrimaryColor(Colors.red);
-              }),
-          MenuItem(
-              label: 'Green',
-              enabled: _primaryColor != Colors.green,
-              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
-                  LogicalKeyboardKey.alt, LogicalKeyboardKey.keyG),
-              onClicked: () {
-                setPrimaryColor(Colors.green);
-              }),
-          MenuItem(
-              label: 'Purple',
-              enabled: _primaryColor != Colors.deepPurple,
-              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
-                  LogicalKeyboardKey.control, LogicalKeyboardKey.keyP),
-              onClicked: () {
-                setPrimaryColor(Colors.deepPurple);
-              }),
-        ])
-      ]),
-      Submenu(label: 'Counter', children: [
-        MenuItem(
-            label: 'Reset',
-            enabled: _counter != 0,
-            shortcut: LogicalKeySet(
-                LogicalKeyboardKey.meta, LogicalKeyboardKey.digit0),
-            onClicked: () {
-              _setCounter(0);
-            }),
-        MenuDivider(),
-        MenuItem(
-            label: 'Increment',
-            shortcut: LogicalKeySet(LogicalKeyboardKey.f2),
-            onClicked: incrementCounter),
-        MenuItem(
-            label: 'Decrement',
-            enabled: _counter > 0,
-            shortcut: LogicalKeySet(LogicalKeyboardKey.f1),
-            onClicked: _decrementCounter),
-      ]),
-    ]);
+//    setApplicationMenu([
+//      Submenu(label: 'Color', children: [
+//        MenuItem(
+//            label: 'Reset',
+//            enabled: _primaryColor != Colors.blue,
+//            shortcut: LogicalKeySet(
+//                LogicalKeyboardKey.meta, LogicalKeyboardKey.backspace),
+//            onClicked: () {
+//              setPrimaryColor(Colors.blue);
+//            }),
+//        MenuDivider(),
+//        Submenu(label: 'Presets', children: [
+//          MenuItem(
+//              label: 'Red',
+//              enabled: _primaryColor != Colors.red,
+//              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
+//                  LogicalKeyboardKey.shift, LogicalKeyboardKey.keyR),
+//              onClicked: () {
+//                setPrimaryColor(Colors.red);
+//              }),
+//          MenuItem(
+//              label: 'Green',
+//              enabled: _primaryColor != Colors.green,
+//              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
+//                  LogicalKeyboardKey.alt, LogicalKeyboardKey.keyG),
+//              onClicked: () {
+//                setPrimaryColor(Colors.green);
+//              }),
+//          MenuItem(
+//              label: 'Purple',
+//              enabled: _primaryColor != Colors.deepPurple,
+//              shortcut: LogicalKeySet(LogicalKeyboardKey.meta,
+//                  LogicalKeyboardKey.control, LogicalKeyboardKey.keyP),
+//              onClicked: () {
+//                setPrimaryColor(Colors.deepPurple);
+//              }),
+//        ])
+//      ]),
+//      Submenu(label: 'Counter', children: [
+//        MenuItem(
+//            label: 'Reset',
+//            enabled: _counter != 0,
+//            shortcut: LogicalKeySet(
+//                LogicalKeyboardKey.meta, LogicalKeyboardKey.digit0),
+//            onClicked: () {
+//              _setCounter(0);
+//            }),
+//        MenuDivider(),
+//        MenuItem(
+//            label: 'Increment',
+//            shortcut: LogicalKeySet(LogicalKeyboardKey.f2),
+//            onClicked: incrementCounter),
+//        MenuItem(
+//            label: 'Decrement',
+//            enabled: _counter > 0,
+//            shortcut: LogicalKeySet(LogicalKeyboardKey.f1),
+//            onClicked: _decrementCounter),
+//      ]),
+//    ]);
   }
 
   @override
@@ -193,33 +191,33 @@ class _MyHomePage extends StatelessWidget {
                       '$counter',
                       style: Theme.of(context).textTheme.headline4,
                     ),
-                    TextInputTestWidget(),
-                    new ElevatedButton(
-                      child: new Text('Test raw keyboard events'),
-                      onPressed: () {
-                        Navigator.of(context).push(new MaterialPageRoute(
-                            builder: (context) => KeyboardTestPage()));
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 380.0,
-                        height: 100.0,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey, width: 1.0)),
-                        child: Scrollbar(
-                          child: ListView.builder(
-                            padding: EdgeInsets.all(8.0),
-                            itemExtent: 20.0,
-                            itemCount: 50,
-                            itemBuilder: (context, index) {
-                              return Text('entry $index');
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
+//                    TextInputTestWidget(),
+//                    new ElevatedButton(
+//                      child: new Text('Test raw keyboard events'),
+//                      onPressed: () {
+//                        Navigator.of(context).push(new MaterialPageRoute(
+//                            builder: (context) => KeyboardTestPage()));
+//                      },
+//                    ),
+//                    Padding(
+//                      padding: const EdgeInsets.all(8.0),
+//                      child: Container(
+//                        width: 380.0,
+//                        height: 100.0,
+//                        decoration: BoxDecoration(
+//                            border: Border.all(color: Colors.grey, width: 1.0)),
+//                        child: Scrollbar(
+//                          child: ListView.builder(
+//                            padding: EdgeInsets.all(8.0),
+//                            itemExtent: 20.0,
+//                            itemCount: 50,
+//                            itemBuilder: (context, index) {
+//                              return Text('entry $index');
+//                            },
+//                          ),
+//                        ),
+//                      ),
+//                    ),
                   ],
                 ),
               ),
